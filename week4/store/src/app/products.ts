4da3a32b02bbc12dc72cff955b20b9fdfea917d7
Alikhan Bissakov <bissakov@yandex.ru>
2021-02-24 11:41:15 +0600

    ,

---
+++ week4/store/src/app/products.ts
@@ -0,0 +1,28 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Apple Lightning to 3.5 mm Headphone Jack Adapter',
+    price: 6.99,
+    description: 'This adapter lets you connect devices that use a 3.5 mm audio plug to your Lightning devices. ' +
+      'Works with all devices that have a Lightning connector and support iOS 10 or later, including iPod touch, iPad, and iPhone.'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'NETGEAR Wi-Fi Range Extender',
+    price: 46.99,
+    description: 'Extended wireless coverage: Adds WiFi range coverage up to 1000 square feet, and connects up to 15 devices such as laptops, smartphones, speakers, IP cameras, tablets, IoT devices, and more'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
